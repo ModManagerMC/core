a1638b7bcf9aec12a903138aaba0aa3ce44258bb
deathsgun <deathsgun@protonmail.com>
2022-08-11 09:50:35 +0200

    Update copyright

--- src/main/kotlin/net/modmanagermc/core/controller/ModInfoController.kt
+++ src/main/kotlin/net/modmanagermc/core/controller/ModInfoController.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2022 DeathsGun
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.modmanagermc.core.controller
 
 import kotlinx.coroutines.*
@@ -6,17 +22,16 @@ import net.modmanagermc.core.mod.IModService
 import net.modmanagermc.core.mod.State
 import net.modmanagermc.core.model.Mod
 import net.modmanagermc.core.store.IStoreService
-import kotlin.time.Duration.Companion.seconds
 import kotlin.time.ExperimentalTime
 
 class ModInfoController(var mod: Mod, private val view: View) {
 
-    val state: State get() = modService.getModState(mod.id)
+    private val state: State get() = modService.getModState(mod.id)
     private val modService: IModService by Core.di
     private val storeService: IStoreService by Core.di
     private var loading = false
 
-    @OptIn(DelicateCoroutinesApi::class, ExperimentalTime::class)
+    @OptIn(DelicateCoroutinesApi::class)
     fun init() {
         loading = true
         GlobalScope.launch(Dispatchers.IO) {
@@ -25,6 +40,11 @@ class ModInfoController(var mod: Mod, private val view: View) {
                 view.setLoading(true)
             }
         }
+        val actionText = when (state) {
+            State.INSTALLED, State.OUTDATED -> "modmanager.button.remove"
+            State.DOWNLOADABLE -> "modmanager.button.install"
+        }
+        view.updateActionText(actionText)
         GlobalScope.launch(Dispatchers.IO) {
             try {
                 mod = storeService.getMod(mod) ?: throw Exception("Unknown error")
@@ -40,7 +60,6 @@ class ModInfoController(var mod: Mod, private val view: View) {
     }
 
     fun doAction() {
-        view.updateActionText("modmanager.button.install")
     }
 
     interface View {
